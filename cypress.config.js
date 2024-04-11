d99c8e1d25ee0f277ba69cfc11c04e13434f403f
Zentaa1 <xprozenta@gmail.com>
2024-04-11 16:38:56 +0200

    Fixed Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-<<<<<<< Updated upstream
-     
-=======
       // implement node event listeners here
->>>>>>> Stashed changes
     },
   },
 });

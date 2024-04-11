d94292fa353dcff8102736c30e6ca0a09506efc6
Zentaa1 <xprozenta@gmail.com>
2024-04-11 15:46:54 +0200

    Added Unit testing

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+<<<<<<< Updated upstream
      
+=======
+      // implement node event listeners here
+>>>>>>> Stashed changes
     },
   },
 });
